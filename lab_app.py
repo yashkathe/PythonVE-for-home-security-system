e7ae8af35f87cb85f416b4fe292f3389c9ecebd3
Yash Kathe <yashkathe123@gmail.com>
2021-04-16 07:00:39 +0100

    section 10 done

--- lab_app.py
+++ lab_app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, request, render_template
 import time
 import datetime
@@ -14,7 +15,6 @@ def hello():
 
 @app.route("/lab_temp")
 def lab_temp():
-	
 	humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 17)
 	if humidity is not None and temperature is not None:
 		return render_template("lab_temp.html",temp=temperature,hum=humidity)
@@ -24,8 +24,13 @@ def lab_temp():
 @app.route("/lab_env_db", methods=['GET']) 
 def lab_env_db():
 	temperatures, humidities, from_date_str, to_date_str = get_records()
-	return render_template("lab_env_db.html",temp=temperatures,hum=humidities)
-
+	return render_template(	"lab_env_db.html", 	temp 			= temperatures,
+							hum 			= humidities,
+							from_date 		= from_date_str, 
+							to_date 		= to_date_str,
+							temp_items 		= len(temperatures),
+							hum_items 		= len(humidities))
+	
 def get_records():
 	from_date_str 	= request.args.get('from',time.strftime("%Y-%m-%d 00:00")) #Get the from date value from the URL
 	to_date_str 	= request.args.get('to',time.strftime("%Y-%m-%d %H:%M"))   #Get the to date value from the URL
@@ -43,14 +48,14 @@ def get_records():
 	if not validate_date(to_date_str):
 		to_date_str 	= time.strftime("%Y-%m-%d %H:%M")		# Validate date before sending it to the DB
 
-	# If range_h is defined, we don't need the from and to times
+		# If range_h is defined, we don't need the from and to times
 	if isinstance(range_h_int,int):	
 		time_now		= datetime.datetime.now()
 		time_from 		= time_now - datetime.timedelta(hours = range_h_int)
 		time_to   		= time_now
 		from_date_str   = time_from.strftime("%Y-%m-%d %H:%M")
 		to_date_str	    = time_to.strftime("%Y-%m-%d %H:%M")
-
+	
 	conn=sqlite3.connect('/var/www/lab_app/lab_app.db')
 	curs=conn.cursor()
 	curs.execute("SELECT * FROM temperatures WHERE rDateTime BETWEEN ? AND ?", (from_date_str, to_date_str))
